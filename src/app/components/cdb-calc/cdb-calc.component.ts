6f97af705cf4284963a3a8dd8caffc1343722550
Marcelo <marcelovqvd@gmail.com>
2024-02-22 12:28:58 -0300

    feat: form wip

--- src/app/components/cdb-calc/cdb-calc.component.ts
+++ src/app/components/cdb-calc/cdb-calc.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-cdb-calc',
   standalone: true,
-  imports: [],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
   templateUrl: './cdb-calc.component.html',
   styleUrl: './cdb-calc.component.scss',
-  template: `Hello {{ city }}`,
 })
 export class CdbCalcComponent {
-  city = 'San Francisco';
+  valueForm!: FormGroup;
+
+  constructor() {
+    this.valueForm = new FormGroup({
+      value: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.valueForm.value);
+  }
 }
